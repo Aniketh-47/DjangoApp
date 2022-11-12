3ee5d1a2ebb415e692a8230e02f818f256e73ec4
Aniketh-47 <aniketmahadik346@gmail.com>
2022-11-12 15:47:49 +0530

    DjangoApp

---
+++ elite/toyapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from toyapp import views
+
+
+urlpatterns = [
+    path("",views.index,name='toyapp'),
+    path("register",views.register,name='registerU'),
+    path("login",views.login,name='loginU'),
+    path("user",views.user,name='User')
+]
